76dc43ca9cbc1843adea4f07155fc7b39b306a90
Hanlinfei <hnlnfi@163.com>
2022-04-07 15:32:47 +0800

    master

--- 58-响应式函数的封装逻辑4.js
+++ 58-响应式函数的封装逻辑4.js
@@ -9,27 +9,36 @@ const obj = {
 
 const targetMap = new WeakMap()
 function getDepend(target, key) {
+    //根据target对象获取map的过程
+    console.log(target);
     let map = targetMap.get(target)
+    console.log(map);
     if (!map) {
         map = new Map()
+        console.log(map);
         targetMap.set(target, map)
-        console.log(targetMap);
-    }
-    let depend = map.get(key)
-    if (!depend) {
-        depend = new Depend()
-        map.set(key, depend)
     }
     console.log(targetMap);
+    console.log(map);
+    //根据key获取depend对象
+
+    let depend = map.get(key)
+    console.log(depend);
+    if (!depend) {
+        depend = new Depend()
+        console.log(depend);
+        map.set(key, depend)
+        console.log(map);
+    }
     return depend
 }
+
 
 const objProxy = new Proxy(obj, {
     get(target, key, receiver) {
         return Reflect.get(target, key, receiver)
     },
     set(target, key, newValue, receiver) {
-        console.log('被设置了');
         Reflect.set(target, key, newValue, receiver)
         getDepend(target, key)
     }
@@ -64,7 +73,7 @@ watchFn(() => {
 })
 
 objProxy.age = 22
-
+objProxy.name = 'linfeizi'
 //我们发现 当数据我们发生变化了之后 他就会去执行notify方法 这时候就会产生一个问题：会把所有被监听的代码块
 //都执行一遍，很显然不符合逻辑，我们只需要把被改变数据的代码块响应式的执行一遍 而不是所有的 所以我们需要进一步区分
 
